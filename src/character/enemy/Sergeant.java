c4c463c6e95eed9c3c77aa450244ac851f5ee199
Eetu Hyvärinen <eetu_hyvarinen@hotmail.com>
2021-06-19 22:49:10 +0200

    added some sounds and refactored code

--- src/character/enemy/Sergeant.java
+++ src/character/enemy/Sergeant.java
@@ -5,13 +5,16 @@ import character.GameCharacter;
 import character.avatar.Avatar;
 import character.avatar.EnemyBullet;
 import game.A_Const;
+import game.Audio;
 
 public class Sergeant extends GameCharacter {
 	private double startTime = 0;
 	private double fireRate = 0.5;
-
+	protected String deathSound = "audio/laser.wav";
+	private String shootingSound = "audio/laser.wav";
 	public Sergeant(double x, double y) {
 		super(x, y, 10, 40, 30, 100, Color.CYAN);
+		super.deathSound = this.deathSound;
 	}
 
 	public void move(double diffSeconds) {
@@ -29,11 +32,14 @@ public class Sergeant extends GameCharacter {
 			double px = avatar.getX()+radius;
 			double py = avatar.getY();
 			double alfa = Math.atan2(py - y, px - x);
-			if (Math.abs(alfa) > 1.3)	handler.addObject(new EnemyBullet(x - radius, y, alfa ,250, 10, 1, Color.blue));
+			if (Math.abs(alfa) > 1.3) {
+				handler.addObject(new EnemyBullet(x - radius, y, alfa ,250, 10, 1, Color.blue));
+				Audio.playSound(shootingSound);
+			}
 			startTime = 0;
 		}
 	}
-
+	
 	@Override
 	public int type() {
 		return A_Const.TYPE_ENEMY;
